2f05092412128f9d4c0d45c264e61bf57b7471a4
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-02-15 16:22:17 +0900

    出品

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,23 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :description
+    validates :image
+  end
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipping_days_id
-    validates :image
   end
+
   VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze    
   validates :price, presence: true, inclusion: { in: (300..9999999)}, format: { with: VALID_PRICE_REGEX }   
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_days
 end
