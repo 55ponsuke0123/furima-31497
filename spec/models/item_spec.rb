5bc22ddfac3c0e39c43c197a3ad209f4d80c169b
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-02-18 12:12:11 +0900

    修正１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,16 +12,21 @@ describe '商品出品機能' do
       expect(@item).to be_valid
     end
     it 'priceが300~9,999,999の間であれば出品できる' do
-      @item.price = '3000'
+      @item.price = 3000
       expect(@item).to be_valid
     end
     it 'priceが半角数字であれば出品できる' do
-      @item.price = '3000'
+      @item.price = 3000
       expect(@item).to be_valid
     end
   end
 
   context '商品出品がうまくいかない時' do
+    it 'imageが空だと出品できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Image can't be blank"
+    end
     it 'titleが空だと出品できない' do
       @item.title = ''
       @item.valid?
@@ -38,7 +43,7 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "Category can't be blank"
     end
     it 'category_idの番号が0だと出品できない' do
-      @item.category_id = '0'
+      @item.category_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include "Category must be other than 0"
     end
@@ -48,7 +53,7 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "State can't be blank"
     end
     it 'state_idの番号が0だと出品できない' do
-      @item.state_id = '0'
+      @item.state_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include "State must be other than 0"
     end
@@ -58,7 +63,7 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "Delivery fee can't be blank"
     end
     it 'delivery_fee_idの番号が0だと出品できない' do
-      @item.delivery_fee_id = '0'
+      @item.delivery_fee_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include "Delivery fee must be other than 0"
     end
@@ -68,7 +73,7 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "Prefecture can't be blank"
     end
     it 'prefecture_idの番号が0だと出品できない' do
-      @item.prefecture_id = '0'
+      @item.prefecture_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include "Prefecture must be other than 0"
     end
@@ -78,7 +83,7 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "Shipping days can't be blank"
     end
     it 'shipping_days_idの番号が0だと出品できない' do
-      @item.shipping_days_id = '0'
+      @item.shipping_days_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include "Shipping days must be other than 0"
     end
@@ -88,12 +93,12 @@ describe '商品出品機能' do
       expect(@item.errors.full_messages).to include "Price can't be blank"
     end
     it 'priceが¥300以下だと出品できない' do
-      @item.price = '200'
+      @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include "Price is not included in the list"
     end
     it 'priceが¥10,000,000以上だと出品できない' do
-      @item.price = '20000000000'
+      @item.price = 20000000000
       @item.valid?
       expect(@item.errors.full_messages).to include "Price is not included in the list"
     end
@@ -102,12 +107,22 @@ describe '商品出品機能' do
       @item.valid?
       expect(@item.errors.full_messages).to include "Price is not included in the list"
     end
+    it 'priceが半角英数混合では出品できない' do
+      @item.price = 'u1200'
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price is not included in the list"
+    end
+    it 'priceが半角英語だけでは出品できない' do
+      @item.price = 'ejikhn'
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price is not included in the list"
+    end
     it 'ユーザーが紐づいていないと商品は出品できない' do
       @item.user = nil
       @item.valid?
       expect(@item.errors.full_messages).to include('User must exist')
+    end
    end
   end
- end
  end
 end
