749f2bbc2530db6460e8e1dd394f44c01877f90d
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-02-20 14:08:54 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  
+  
   def index
+    @items = Item.includes(:user).order(created_at: :desc) 
+    @item = Item.find_by(params[:user_id])
   end
+
 
   def new
     @item = Item.new
@@ -20,6 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,17 @@ class Item < ApplicationRecord
     validates :description
     validates :image
   end
+
+  VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze    
+  validates :price, presence: true, inclusion: { in: (300..9999999)}, format: { with: VALID_PRICE_REGEX }   
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_days
+
   with_options presence: true, numericality: { other_than: 0 } do
     validates :category_id
     validates :state_id
@@ -15,14 +26,5 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_days_id
   end
-
-  VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze    
-  validates :price, presence: true, inclusion: { in: (300..9999999)}, format: { with: VALID_PRICE_REGEX }   
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :state
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_days
+  
 end
