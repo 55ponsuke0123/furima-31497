a7147b9b6c85514454b268fe12f7674eeb9d2e4c
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-02-22 08:27:18 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+       if @item.update(item_params)
+        redirect_to item_path
+       else
+       render :edit
+       end
   end
 
   private
